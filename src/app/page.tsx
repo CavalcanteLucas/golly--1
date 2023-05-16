5bee5a7d6b8f822fe0d2ecee6710f22d2ed31490
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-05-16 15:08:01 -0300

    Draw board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
+import { createElement } from "react";
+
+
+let cell = <td className="cell"></td>
+
+function Row({ size=10 }) {
+
+  let row = [];
+  for (let i = 0; i < size; i++) {
+    row.push(
+      cell
+    )
+  }
+  return <>{row}</>;
+}
+
+function Board({ size=10 }) {
+
+  let board = [];
+  for (let i = 0; i < size; i++) {
+    board.push(      
+      <tr><Row size={size}/></tr>
+    )
+  }
+  return <>{board}</>;
+}
+
 export default function Home() {
+
+  let tbody = <tbody><Board size={10}/></tbody>
+
+  let table = <table className="board">{tbody}</table>
+
   return (
     <main>
-      <h1>ola marilene</h1>
+      <div className="content">
+        {table}
+      </div>
     </main>
-  )
+  );
 }
